f83ac97751a111b0bf7e7de54483f5662983a81e
Mike Vanbuskirk <mvanbus43@gmail.com>
2019-11-12 23:15:02 -0500

    refactor schema to account for list

--- azuredevops/data_projects.go
+++ azuredevops/data_projects.go
@@ -6,23 +6,40 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/microsoft/azure-devops-go-api/azuredevops/core"
+	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/utils/config"
 )
 
 func dataProjects() *schema.Resource {
 	return &schema.Resource{
 		Read: dataSourceProjectsRead,
 		Schema: map[string]*schema.Schema{
-			"name": {
-				Type:         schema.TypeString,
-				ForceNew:     true,
-				Optional:     true,
-				ValidateFunc: validation.NoZeroValues,
-			},
-			"state": {
-				Type:         schema.TypeString,
-				ForceNew:     true,
-				Optional:     true,
-				ValidateFunc: validation.NoZeroValues,
+			"projects": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Type:         schema.TypeString,
+							Computed:     true,
+							ValidateFunc: validation.NoZeroValues,
+						},
+						"project_id": {
+							Type:         schema.TypeString,
+							Computed:     true,
+							ValidateFunc: validation.NoZeroValues,
+						},
+						"project_url": {
+							Type:         schema.TypeString,
+							Computed:     true,
+							ValidateFunc: validation.NoZeroValues,
+						},
+						"state": {
+							Type:         schema.TypeString,
+							ValidateFunc: validation.NoZeroValues,
+							Default:      "WellFormed",
+						},
+					},
+				},
 			},
 		},
 	}
@@ -32,17 +49,41 @@ func dataProjects() *schema.Resource {
 //  (1) Query for all Projects that exist within the organization.
 //  (2) If a state filter is provided return only matching projects.
 func dataSourceProjectsRead(d *schema.ResourceData, m interface{}) error {
-	clients = m.(*aggregatedClient)
+	clients := m.(*config.AggregatedClient)
+	state := d.Get("state").(string)
+
+	projects, err := getProjectsForState(clients, state)
+	if err != nil {
+		return fmt.Errorf("Error finding projects with state %s. Error: %v", state, err)
+	}
 	return nil
 }
 
-func getProjectsWithContinuationToken(clients *aggregatedClient, continuationToken string) ([]core.TeamProjectReference, string, error) {
-	args := core.GetProjectsArgs{}
+func getProjectsForState(clients *config.AggregatedClient, projectState string) ([]core.TeamProjectReference, error) {
+	var projects []core.TeamProjectReference
+	var currentToken string
+
+	for hasMore := true; hasMore; {
+		newProjects, latestToken, err := getProjectsWithContinuationToken(clients, projectState, currentToken)
+		currentToken = latestToken
+		if err != nil {
+			return nil, err
+		}
+
+		projects = append(projects, newProjects...)
+		hasMore = currentToken != ""
+	}
+
+	return projects, nil
+}
+
+func getProjectsWithContinuationToken(clients *config.AggregatedClient, projectState string, continuationToken string) ([]core.TeamProjectReference, string, error) {
+	args := core.GetProjectsArgs{StateFilter: &projectState}
 	if continuationToken != "" {
 		args.ContinuationToken = &continuationToken
 	}
 
-	response, err := clients.CoreClient.GetProjects(clients.ctx, args)
+	response, err := clients.CoreClient.GetProjects(clients.Ctx, args)
 	if err != nil {
 		return nil, "", err
 	}
